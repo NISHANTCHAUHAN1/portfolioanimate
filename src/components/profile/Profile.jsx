7d0c26e200cb83780d28764e2226348a8d97a3a8
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-09-13 00:44:25 -0400

    fix

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -5,7 +5,7 @@ import nish from '../../assets/portfolio-img3.webp';
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 
-import pdf from "../../assets/NishResume.pdf";
+import pdf from "../../assets/resume.pdf";
 const ProfileSection = () => {
   const handleResumeClick = () => {
     window.open(pdf);
