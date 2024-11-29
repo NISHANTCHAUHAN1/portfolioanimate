453ffa9b06c7d88abc8ec8eafbc360acb62ee7f7
NISHANTCHAUHAN1 <onenish14@gmail.com>
2024-11-29 02:01:01 -0500

    changes

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,5 @@
 import "./profile.css"
 import nish from '../../assets/portfolio-img3.webp';
-// import img from '../../assets/portfolio-img3.webp';
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 
